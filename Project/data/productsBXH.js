c5836b043ea3c20a080f4594f3496405f8ffaa7f
Ngocvudut1995 <Ngocvudut1995@gmail.com>
2015-10-18 21:11:25 +0700

    Đồng bộ mới.

---
+++ Project/data/productsBXH.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Vu Dang on 10/18/2015.
+ */
+var products = []; // Khai bao 1 mang rong.
+
+
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
+
