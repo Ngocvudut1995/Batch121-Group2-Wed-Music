572b7c3cb2af22c94794f2ea81cc1c301d93a0fd
Levantu710 <letu710@gmail.com>
2015-10-20 22:04:24 +0700

    new

--- Project/data/productsPlaylist.js
+++ Project/data/productsPlaylist.js
@@ -10,3 +10,33 @@ playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài
 playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
 playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
 playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+
+
+var mvIndex =[];
+mvIndex.push({img:'images/Playlist/index/download.jpg', title:'MV về ....',name:'Hoàng Yến Chi BI'});
+mvIndex.push({img:'images/Playlist/index/issac.jpg', title:'MV về ....',name:'Issac'});
+mvIndex.push({img:'images/Playlist/index/bxh6.jpg', title:'MV về ....',name:'Issac'});
+mvIndex.push({img:'images/Playlist/index/download.jpg', title:'MV về ....',name:'Hoàng Yến Chi BI'});
+mvIndex.push({img:'images/Playlist/index/download.jpg', title:'MV về ....',name:'Hoàng Yến Chi BI'});
+mvIndex.push({img:'images/Playlist/index/issac.jpg', title:'MV về ....',name:'Issac'});
+mvIndex.push({img:'images/Playlist/index/bxh6.jpg', title:'MV về ....',name:'Issac'});
+mvIndex.push({img:'images/Playlist/index/download.jpg', title:'MV về ....',name:'Hoàng Yến Chi BI'});
+
+
+
+var caKhucHotIndex=[];
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+caKhucHotIndex.push({music:'tên ca khuc', title:'lượt nghe',name:'ca sĩ'});
+
