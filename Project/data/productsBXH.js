c47afdf0d82afc09150cd34122b4d73117de5f6a
Ngocvudut1995 <Ngocvudut1995@gmail.com>
2015-10-20 18:17:21 +0700

    Đồng bộ lần tiếp theo.

--- Project/data/productsBXH.js
+++ Project/data/productsBXH.js
@@ -17,3 +17,9 @@ products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm
 products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
 products.push({imgSrc: 'images/BXH/Sontung.jpg', name: 'Sơn Tùng', title: 'Âm Thầm Bên Em'});
 
+var baihathanquoc =[];
+baihathanquoc.push({img:'images/BXH/Han Quoc/bigbang.jpg',name:'Big Bang', title:'Let`s Not Fall In Love'});
+baihathanquoc.push({img:'images/BXH/Han Quoc/bigbang.jpg',name:'Big Bang', title:'Let`s Not Fall In Love'});
+baihathanquoc.push({img:'images/BXH/Han Quoc/bigbang.jpg',name:'Big Bang', title:'Let`s Not Fall In Love'});
+baihathanquoc.push({img:'images/BXH/Han Quoc/bigbang.jpg',name:'Big Bang', title:'Let`s Not Fall In Love'});
+baihathanquoc.push({img:'images/BXH/Han Quoc/bigbang.jpg',name:'Big Bang', title:'Let`s Not Fall In Love'});
---
+++ Project/data/productsPlaylist.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vu Dang on 10/20/2015.
+ */
+var playlistIndex =[];
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
+playlistIndex.push({img:'images/Playlist/index/phunuvn.jpg', title:'Những bài bát về ....',name:'Various Artists'});
