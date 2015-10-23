ae2829016ab8104465268b1836b02cd92bc7e60c
Levantu710 <letu710@gmail.com>
2015-10-23 22:17:12 +0700

    chude--video---...

---
+++ Project/data/pageChuDe.js
@@ -0,0 +1,16 @@
+/**
+ * Created by TU on 10/23/2015.
+ */
+var page=[];
+page.push({src:'images/chude/hot.jpg',name:'Nhạc Hot Việt'});
+page.push({src:'images/chude/moi.jpg',name:'Nhạc Việt Mới'});
+page.push({src:'images/chude/us.jpg',name:'Nhạc US-UK'});
+page.push({src:'images/chude/han.jpg',name:'Nhạc HÀn'});
+page.push({src:'images/chude/rapViet.jpg',name:'Nhạc Rap Việt'});
+page.push({src:'images/chude/hoaAm.jpg',name:'HÒa Âm ánh Sáng'});
+page.push({src:'images/chude/truTinh.jpg',name:'Nhạc Trữ TÌnh'});
+page.push({src:'images/chude/batHu.jpg',name:'Nhạc Bất Hủ'});
+page.push({src:'images/chude/nagyCuoi.jpg',name:'Nhạc Ngày Cưới'});
+page.push({src:'images/chude/meBe.jpg',name:'Nhạc MẸ và BÉ'});
+page.push({src:'images/chude/nhacCho.jpg',name:'Nhạc Chờ'});
+page.push({src:'images/chude/hocTro.jpg',name:'Tuổi Học Trò'});
---
+++ Project/data/pagePlayList.js
@@ -0,0 +1,21 @@
+/**
+ * Created by TU on 10/21/2015.
+ */
+var pagePlayList=[];
+pagePlayList.push({name:'Playlist MỚi'});
+pagePlayList.push({name:"Nhạc Trịnh"});
+pagePlayList.push({name:"Trữ Tình"});
+pagePlayList.push({name:"CÁch Mạng"});
+pagePlayList.push({name:"Tiền Chiến"});
+pagePlayList.push({name:"Nhạc Trịnh"});
+pagePlayList.push({name:"Thiếu Nhi"});
+pagePlayList.push({name:"Rap Việt"});
+pagePlayList.push({name:"Rock Việt"});
+pagePlayList.push({name:"Hàn Quốc "});
+pagePlayList.push({name:"Nhạc Hoa"});
+pagePlayList.push({name:"Nhạc Nhật"});
+pagePlayList.push({name:"KhÔng Lời"});
+pagePlayList.push({name:"Nhạc Phim"});
+pagePlayList.push({name:"Thể Loại Khác"});
+
+
